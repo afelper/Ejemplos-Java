07134dd52a43b4d7b9bb3ad672b9be6c33ef3900
afelper <afpm82@gmail.com>
2021-06-28 23:39:04 -0500

    Correcion1 Ortografia

--- Practica1/Longitud_Area/src/LongitudArea.java
+++ Practica1/Longitud_Area/src/LongitudArea.java
@@ -4,11 +4,11 @@ public class LongitudArea {
     public static void main(String[] args) throws Exception {
        Scanner sc = new Scanner(System.in);
        double radio, longitud, area;
-       System.out.println("Intro rad de la circu: ");
+       System.out.println("Intro radio de la circuferencia: ");
        radio = sc.nextDouble();
        longitud = 2 * Math.pow(radio, 2);
        area = Math.PI * Math.pow(radio, 2);
-       System.out.println("Longitud de la circ ->"+ longitud);
+       System.out.println("Longitud de la circuferenica ->"+ longitud);
        System.out.println("Área de la circuferencia -> " + area);
 
     }
